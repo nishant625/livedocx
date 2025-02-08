1d92c4c6721322256f02e5ea757e1e3df5073e6b
nishant625 <gangwarnishant625@gmail.com>
2025-02-08 15:04:01 +0530

    working on the collab implementation

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,25 +1,32 @@
-"user client"
+"use client"
 
 import { ClientSideSuspense, RoomProvider } from '@liveblocks/react'
 import React from 'react'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/nextjs'
-const CollaborativeRoom = () => {
+import ActiveCollaborators from './ActiveCollaborators'
+const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
     return (
-        <RoomProvider id="my-room">
+
+        // {roomId:string,roomMetadata:string}
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
                 <div className="collaborative-room">
                     <Header>
                         <div className='flex w-fit items-center justify-center gap-2'>
                             <p className='document-title'>this is a fake title</p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+                        <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
+
                     </Header>
                     <Editor />
                 </div>
